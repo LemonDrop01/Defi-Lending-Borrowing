34b30fb19df62fa0b968074ea4c4866d0409b182
LemonDrop01 <97831934+LemonDrop01@users.noreply.github.com>
2024-11-18 17:48:39 -0600

    Main

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-    content: [
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["C:\Users\tgood\Documents\GitHub\Defi-Lending-Borrowing\styles\globals.css.{html,js}"],
   theme: {
     extend: {},
   },
